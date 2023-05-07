685e2b4da40cd88d633fdf28d5febf824ebeaaa1
YiBiNeUtRoN <jm11sevilla@gmail.com>
2023-05-07 22:30:41 +0200

    commit añadir interacción con placa arduino

---
+++ src/arduino.js
@@ -0,0 +1,27 @@
+const { SerialPort } = require('serialport');
+const comPort1 = new SerialPort({
+  path: 'COM3',
+  baudRate: 9600,
+  dataBits: 8,
+  stopBits: 1,
+  parity: 'none',
+});
+
+comPort1.on('open', function () {
+  console.log('Conexión establecida correctamente');
+});
+
+comPort1.on('error', function (err) {
+  console.error('Error al conectar: ', err.message);
+});
+
+comPort1.on('data', function (data) {
+  // Convertir el buffer recibido a un string
+  let str = data.toString();
+
+  // Parsear el valor del pin analógico
+  let valor = parseInt(str);
+
+  // Mostrar el valor leído en la consola
+  console.log('Valor leído: ' + valor);
+});
