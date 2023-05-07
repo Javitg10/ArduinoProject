a41db359a0568da25599cd22927d889ac9b1ab9a
javitg10 <javiertg84@gmail.com>
2023-05-07 19:19:05 +0200

    commit header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/components/home/home.component';
+import { AboutUsComponent } from './core/components/about-us/about-us.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:'about', component:AboutUsComponent}
 ];
 
 @NgModule({
