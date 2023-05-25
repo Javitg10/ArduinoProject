32823ce8e2043c4ae826772dd8c9dc1875654571
javitg10 <javiertg84@gmail.com>
2023-05-25 22:11:18 +0200

    Retoques estetica

--- src/app/core/components/datos/datos.component.ts
+++ src/app/core/components/datos/datos.component.ts
@@ -16,15 +16,43 @@ export class DatosComponent {
     let url = 'http://localhost:3000/api/datos'
     this.datos$ = this.http.get<number[]>(url);
   }
+
+  iniciarServidor() {
+    const url = 'http://localhost:3000';
+    this.http.get(url).subscribe(
+      response => {
+        console.log('Servidor iniciado correctamente');
+      },
+      error => {
+        console.error('Error al iniciar el servidor', error);
+      }
+    );
+  }
+
+  iniciarRecoleccionDatos() {
+    const url = 'http://localhost:3000/api/datos';
+    this.http.get<number[]>(url).subscribe(
+      data => {
+        console.log('Datos recibidos:', data);
+        // Aquí puedes realizar cualquier otra acción con los datos recibidos
+      },
+      error => {
+        console.error('Error al obtener los datos', error);
+      }
+    );
+  }
   ejecutarArchivo() {
-    const url = 'http://localhost:3000/ejecutar-archivo'; // Reemplaza con la URL de tu servidor
-  this.http.get(url).subscribe(
-    (response) => {
-      console.log('Archivo ejecutado correctamente');
-    },
-    (error) => {
-      console.error('Error al ejecutar el archivo', error);
-    }
-  );
+    const url = 'http://localhost:3000/api/ejecutar-archivo'; // Reemplaza con la URL de tu servidor
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          console.log('Archivo ejecutado correctamente');
+        } else {
+          console.error('Error al ejecutar el archivo');
+        }
+      })
+      .catch(error => {
+        console.error('Error al ejecutar el archivo', error);
+      });
   }
 }
