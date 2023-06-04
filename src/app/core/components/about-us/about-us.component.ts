46f193f694a61504e84b847d78060688d7c5e7bb
javitg10 <javiertg84@gmail.com>
2023-06-04 19:11:27 +0200

    Commit de estilos

--- src/app/core/components/about-us/about-us.component.ts
+++ src/app/core/components/about-us/about-us.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
+
+  @ViewChild('demoElement') demoElement!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+    showCardSection: boolean = false;
+  
+    showCards() {
+      this.showCardSection = true;
+    }
+  }
