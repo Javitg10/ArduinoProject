20f377ccf9dd9167709a684a3beb1da39b9c2b08
Jose 2 <jm11sevilla@gmail.com>
2023-06-12 13:37:33 +0200

    CommitBBDD2

--- BBDD/arduino_datos.sql
+++ BBDD/arduino_datos.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `lecturas`
 --
-CREATE DATABASE arduino_datos;
+
 DROP TABLE IF EXISTS `lecturas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
